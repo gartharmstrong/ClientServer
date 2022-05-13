aa89b06a5b1e586583bac145266c7e760ae66e00
sggarmst <sggarmst@liverpool.ac.uk>
2022-05-13 11:26:49 +0800

    Initial check in for client and server

---
+++ client/client.py
@@ -0,0 +1,86 @@
+import socket
+from cryptography.fernet import Fernet
+
+def generate_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+def encrypt_file(filename):
+    key = open("key.key", "rb").read()
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        file_data = file.read()
+    encrypted_data = f.encrypt(file_data)
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
+
+def main():
+    #File type
+    while True:
+        file_type = input("File type: Text file(t) or Dictionary(d)? ")
+        if file_type.lower() == "t":
+            print("Input is text file")
+            break
+        elif file_type.lower() == "d":
+            print("Input is dictionary")
+            break
+        else:
+            print("invalid selection")
+
+    #File Pickling
+    while True:
+        file_pickling = input("Pickling(not implemented)? ")
+        break
+
+    #File encryption
+    while True:
+        file_encrypted = input("Encrypt file (y/n)? ")
+        if file_encrypted.lower() == "y":
+            print("File will be encrypted")
+            break
+        elif file_encrypted.lower() == "n":
+            print("File will not be encrypted")
+            break
+        else:
+            print("invalid selection")
+
+    #File name
+    while True:
+        file_name = input("Enter filename to be sent: ")
+        break
+    
+    #Encrypt file if required
+    if file_encrypted == "y":
+        encrypt_file(file_name)
+
+    # create a socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+    # get local machine name
+    host = socket.gethostname()                           
+
+    port = 29999
+
+    # connection to hostname on the port.
+    s.connect((host, port))                               
+    print("Connected")
+
+    s.send(file_type.encode('utf-8'))
+    s.send(file_pickling.encode('utf-8'))
+    s.send(file_encrypted.encode('utf-8'))
+    s.send(file_name.encode('utf-8'))
+     
+    file = open(file_name,'rb')
+    data = file.read(1024)
+    while (data):
+        s.send(data)
+        data = file.read(1024)
+    file.close()
+
+    s.close()
+
+    print("Completed sending data to server")
+
+if __name__ == "__main__":
+    main()
---
+++ server/server.py
@@ -0,0 +1,100 @@
+import socket                                         
+from cryptography.fernet import Fernet
+import os
+
+def decrypt_file(filename):
+    key = open("key.key", "rb").read()
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        encrypted_data = file.read()
+    decrypted_data = f.decrypt(encrypted_data)
+    with open(filename, "wb") as file:
+        file.write(decrypted_data)
+
+def output_console(file_name):
+    with open(file_name,'r') as viewFileOpen:
+        data = viewFileOpen.read()
+    print(data)
+
+def main():
+    while True:
+        output_type = input("Output to File(f) or Console(c)? ")
+        if output_type.lower() == "f":
+            print("Output to file\n")
+            break
+        elif output_type.lower() == "c":
+            print("Output to console\n")
+            break
+        else:
+            print("invalid selection\n")
+
+    # create a socket object
+    serversocket = socket.socket(
+                socket.AF_INET, socket.SOCK_STREAM)
+
+    # get local machine name
+    host = socket.gethostname()                           
+    port = 29999                                           
+
+    # bind to the port
+    try:
+        serversocket.bind((host, port))
+        print("Socket bind successful")
+    except:
+        print("Unable to bind socket")
+        exit(0)
+
+    # queue up to 5 requests
+    serversocket.listen(5)
+
+    while True:
+        # establish a connection
+        clientsocket,addr = serversocket.accept()      
+        print("Connection received from %s" % str(addr))
+
+        # Receive parameters from client
+        file_type = clientsocket.recv(1).decode('utf-8')
+        file_pickling = clientsocket.recv(1).decode('utf-8')
+        file_encrypted = clientsocket.recv(1).decode('utf-8')
+        file_name = clientsocket.recv(100).decode('utf-8')
+
+        print("\nFile type: " + file_type)
+        print("Pickling: " + file_pickling)
+        print("File encrypted: " + file_encrypted)
+        print("Filename: " + file_name)
+        
+        #Delete previous file
+        if os.path.exists(file_name):
+            os.remove(file_name)
+
+        with open(file_name, 'wb') as f:
+            while True:
+                data = clientsocket.recv(1024)
+                if not data:
+                    break
+                # write data to a file
+                f.write(data)
+        f.close()
+        print("\nReceived data\n")
+
+        if output_type.lower() == "f":
+            print("File written to server")
+
+        #Decrypt file if required
+        if file_encrypted == "y":
+            decrypt_file(file_name)
+
+        #Output to console
+        if output_type.lower() == "c":
+            print("Contents of file:")
+            output_console(file_name)
+            #Remove file - only want it in console
+            if os.path.exists(file_name):
+                os.remove(file_name)
+
+        clientsocket.close()
+        print("\nClosing server")
+        break
+
+if __name__ == "__main__":
+    main()
