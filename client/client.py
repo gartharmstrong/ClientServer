e39319a17ca169e58839299f8b92463288d71473
Ho Him Lam <hohimlam123@gmail.com>
2022-05-21 17:36:30 +0800

    Unit test added

--- client/client.py
+++ client/client.py
@@ -1,3 +1,4 @@
+from argparse import FileType
 import socket
 from cryptography.fernet import Fernet
 import json
@@ -15,6 +16,13 @@ data = {
     'Task':'Send dictionary'
 }
 
+file_type = ""
+msg_pickling = ""
+sendmesg = ""
+file_name = ""
+file_encrypted = ""
+result_string = {}
+
 json_pick = json.dumps(data) # for pickling JSON format 
 binary_pick = pk.dumps(data) # for picking binary format
 xml_pick = Element('data') # to change the data dictionary to an xml file
@@ -29,6 +37,7 @@ def generate_key():
         key_file.write(key)
 
 def encrypt_file(filename):
+    global result_string
     key = open("key.key", "rb").read()
     f = Fernet(key)
     dest = filename + "backup"
@@ -38,105 +47,167 @@ def encrypt_file(filename):
     encrypted_data = f.encrypt(file_data)
     with open(filename, "wb") as file:
         file.write(encrypted_data)
+    result_string["Encrypted"] = encrypted_data
+    print("Encrypted result: ", result_string["Encrypted"])
 
-def main():
-    #File type
-    while True:
-        file_type = input("File type: Text file(t) or Dictionary(d)? ")
-        if file_type.lower() == "t":
-            print("Input is text file")
-            break
-        elif file_type.lower() == "d":
-            print("Input is dictionary")
-            break
-        else:
-            print("invalid selection")
-
-    #File Pickling - only for dictionary
-    if file_type == "d":
+def main(type, format, encrypted):
+    
+    def inputs():
+        global file_type 
+        global msg_pickling 
+        global sendmesg 
+        global file_name
+        global file_encrypted
+        
+        #File type
         while True:
-            msg_pickling = print(f"Choose pickling format")
-            msg_pickling = input("1, JSON\n"
-                    "2, Binary\n"
-                    "3, XML\n"
-                    "Choose 1 for JSON, 2 for Binary format, 3 for XML : ")
-            if (str(msg_pickling) == "1"):
-                file_name = "jsonfile.txt"
-                sendmesg = str.encode(json_pick) # pick for JSON format
+            file_type = input("File type: Text file(t) or Dictionary(d)? ")
+            if file_type.lower() == "t":
+                print("Input is text file")
                 break
-            elif (str(msg_pickling) == "2"):
-                file_name = "binaryfile.txt"
-                sendmesg = binary_pick # pick for binary format
-                break
-            elif (str(msg_pickling) == "3"):
-                file_name = "xmlfile.txt"
-                sendmesg = tostring(xml_pick) # pick for XML format
+            elif file_type.lower() == "d":
+                print("Input is dictionary")
                 break
             else:
                 print("invalid selection")
-    else:
-        msg_pickling="0" #default value if text file
 
-    #File encryption - only for text file
-    if file_type == "t":
-        file_name = "textfile.txt"
-        if os.path.exists(file_name):
+        #File Pickling - only for dictionary
+        if file_type == "d":
             while True:
-                file_encrypted = input("Encrypt file (y/n)? ")
-                if file_encrypted.lower() == "y":
-                    print("File will be encrypted")
+                msg_pickling = print(f"Choose pickling format")
+                msg_pickling = input("1, JSON\n"
+                        "2, Binary\n"
+                        "3, XML\n"
+                        "Choose 1 for JSON, 2 for Binary format, 3 for XML : ")
+                if (str(msg_pickling) == "1"):
+                    file_name = "jsonfile.txt"
+                    sendmesg = str.encode(json_pick) # pick for JSON format
                     break
-                elif file_encrypted.lower() == "n":
-                    print("File will not be encrypted")
+                elif (str(msg_pickling) == "2"):
+                    file_name = "binaryfile.txt"
+                    sendmesg = binary_pick # pick for binary format
+                    break
+                elif (str(msg_pickling) == "3"):
+                    file_name = "xmlfile.txt"
+                    sendmesg = tostring(xml_pick) # pick for XML format
                     break
                 else:
                     print("invalid selection")
         else:
-            sys.exit(file_name + " does not exist.")
+            msg_pickling="0" #default value if text file
+
+        #File encryption - only for text file
+        if file_type == "t":
+            file_name = "textfile.txt"
+            if os.path.exists(file_name):
+                while True:
+                    file_encrypted = input("Encrypt file (y/n)? ")
+                    if file_encrypted.lower() == "y":
+                        print("File will be encrypted")
+                        break
+                    elif file_encrypted.lower() == "n":
+                        print("File will not be encrypted")
+                        break
+                    else:
+                        print("invalid selection")
+            else:
+                sys.exit(file_name + " does not exist.")
+        else:
+            file_encrypted="n" #default value if dictionary
+        
+        #Encrypt file if required
+        if file_encrypted == "y":
+            encrypt_file(file_name)
+
+    if type == "":
+        inputs()
     else:
-        file_encrypted="n" #default value if dictionary
-    
-    #Encrypt file if required
-    if file_encrypted == "y":
-        encrypt_file(file_name)
+        global file_type 
+        global msg_pickling 
+        global sendmesg 
+        global file_name
+        global file_encrypted
 
-    # create a socket object
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        file_type = type
+        msg_pickling = format   
+        if file_type == "d":
+            if (str(msg_pickling) == "1"):
+                file_name = "jsonfile.txt"
+                result_string["Unencrypted"] = json.dumps(data)
+                sendmesg = str.encode(json_pick) # pick for JSON format
+                result_string["Encoded_data"] = sendmesg
+                
+            elif (str(msg_pickling) == "2"):
+                file_name = "binaryfile.txt"
+                result_string["Unencrypted"] = data
+                sendmesg = pk.dumps(data) # pick for binary format
+                result_string["Encoded_data"] = sendmesg
+                
+            elif (str(msg_pickling) == "3"):
+                file_name = "xmlfile.txt"
+                result_string["Unencrypted"] = data
+                sendmesg = tostring(xml_pick) # pick for XML format
+                result_string["Encoded_data"] = sendmesg
+        
+        if file_type == "t":
+            file_name = "textfile.txt"
+            print("[Test] file path: ", file_name)
+                    
+        file_encrypted = encrypted
+        if file_encrypted == "y":
+            encrypt_file(file_name)
 
-    # get local machine name
-    host = socket.gethostname()
-    port = 29999
+    if not type:
+        print("[Client] File type: " + file_type)
+        print("[Client] Msg pickling: " + msg_pickling)
+        print("[Client] Filename: " + file_name)
+        print("[Client] File encrypted?: " + file_encrypted)
 
-    # connection to hostname on the port.
-    s.connect((host, port))                               
-    print("Connected to server")
+    def socketSetUp():
+        global result_string
+        # create a socket object
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
-    s.send(file_type.encode('utf-8'))
-    s.send(msg_pickling.encode('utf-8'))
-    s.send(file_encrypted.encode('utf-8'))
-    s.send(file_name.encode('utf-8'))
+        # get local machine name
+        host = socket.gethostname()
+        port = 29999
 
-    time.sleep(2)
+        # connection to hostname on the port.
+        s.connect((host, port))                               
+        print("Connected to server")
 
-    if file_type == "t":
-        file = open(file_name,'rb')
-        data = file.read(1024)
-        while (data):
-            s.send(data)
+        s.send(file_type.encode('utf-8'))
+        s.send(msg_pickling.encode('utf-8'))
+        s.send(file_encrypted.encode('utf-8'))
+        s.send(file_name.encode('utf-8'))
+
+        time.sleep(2)
+
+        if file_type == "t":
+            file = open(file_name,'rb')
             data = file.read(1024)
-        file.close()
-    elif file_type == "d":
-        print("Pickled data: " + str(sendmesg))
-        s.send(sendmesg)
+            result_string["Unencrypted"] = file_name
+            while (data):
+                s.send(data)
+                data = file.read(1024)
+                print(data)
+                
+            file.close()
+        elif file_type == "d":            
+            s.send(sendmesg)
+            if not type:
+                print("Pickled data: " + str(sendmesg))
 
-    if file_encrypted == "y":
-        backup = file_name + "backup"
-        shutil.copy(backup, file_name)
-        os.remove(backup)
+        if file_encrypted == "y":
+            backup = file_name + "backup"
+            shutil.copy(backup, file_name)
+            os.remove(backup)
 
-    s.close()
+        s.close()
 
-    print("Completed sending data to server")
+        print("Completed sending data to server")
+    socketSetUp()
+    return result_string
 
 if __name__ == "__main__":
-    main()
+    main("","","")
--- server/server.py
+++ server/server.py
@@ -1,10 +1,12 @@
-import socket                                     
+import socket                                  
 from cryptography.fernet import Fernet
 import os
 import json
 import pickle as pk
 import xml.etree.ElementTree as et
 import xmltodict
+
+result_string = {}
 
 def json_format(file_name): #function for json
     with open(file_name, 'r') as pkfile:
@@ -13,6 +15,11 @@ def json_format(file_name): #function for json
         unpkfile.write(str(unpickled))
 
 def json_dict(file_name): #function for json <class 'dict'>
+    #For unittest only
+    global result_string
+    with open(file_name, 'r') as dictfile2:      
+        result_string["Encoded_data"] =  dictfile2.read()
+
     with open(file_name, 'r') as dictfile:
         unpickled = json.loads(dictfile.read())
     return unpickled
@@ -24,7 +31,14 @@ def binary_format(file_name): #function for binary
         unpkfile.write(str(unpickled))
         
 def binary_dict(file_name): #function for binary <class 'dict'>
-    with open(file_name, 'rb') as pkfile:
+    #For unittest only
+    global result_string
+    with open(file_name, 'rb') as pkfile2:      
+        result_string["Encoded_data"] =  pkfile2.read()
+        #print ("[Server] Encoded dictionary: ", result_string["Encoded_data"])
+    
+    #For production
+    with open(file_name, 'rb') as pkfile:        
         unpickled = pk.load(pkfile, encoding='utf-8')
         return unpickled
 
@@ -41,38 +55,51 @@ def xml_format(file_name): # function for XML
         unpkfile.write(str(xml_dict)) # added to code
 
 def xml_format_dict(file_name): # function for XML <class 'dict'>
+    global result_string
     with open(file_name,'r') as pkfile: # added to code
         unpickled = et.parse(pkfile) # added to code
         root = unpickled.getroot()
         write_file = et.tostring(root)
+
+        result_string["Encoded_data"] =  write_file #For unittest
+        #print ("[Client] Encoded dictionary: ", result_string["Encoded_data"])
+
         xml_decode = write_file.decode() # 
         xml_datadict = xmltodict.parse(xml_decode) #change XML to dictionary
         xml_dict = dict(xml_datadict.pop('data'))
         return (xml_dict)
 
 def decrypt_file(filename, output_type):
+    global result_string
     key = open("key.key", "rb").read()
     f = Fernet(key)
     with open(filename, "rb") as file:
         encrypted_data = file.read()
+    result_string["Encrypted"] = encrypted_data
     decrypted_data = f.decrypt(encrypted_data)
     if output_type == "c":
         print("\nDecrypted data: \n" + decrypted_data.decode("utf-8"))
     with open(filename, "wb") as file:
         file.write(decrypted_data)
 
-def output_console(file_name):
+def output_console(file_name):    
     with open(file_name,'r') as viewFileOpen:
         data = viewFileOpen.read()
     print(data)
 
-def main():
+def main(filetype):
+    #for unittest
+    global result_string
+
     # create a socket object
     serversocket = socket.socket(
                 socket.AF_INET, socket.SOCK_STREAM)
 
     # get local machine name
-    host = socket.gethostname()
+    if not filetype:
+        host = socket.gethostname()
+    else:
+        host = '0.0.0.0'
     port = 29999
     # bind to the port
     try:
@@ -82,18 +109,20 @@ def main():
         print("Unable to bind socket")
         exit(0)
     
-    while True:
-        output_type = input("Output to File(f) or Console(c)? ")
-        if output_type.lower() == "f":
-            print("Output to file\n")
-            break
-        elif output_type.lower() == "c":
-            print("Output to console\n")
-            break
-        else:
-            print("invalid selection\n")
-
-
+    if not filetype:
+        while True:
+            output_type = input("Output to File(f) or Console(c)? ")
+            if output_type.lower() == "f":
+                print("Output to file\n")
+                break
+            elif output_type.lower() == "c":
+                print("Output to console\n")
+                break
+            else:
+                print("invalid selection\n")
+    else:
+        output_type = filetype
+    
     # queue up to 5 requests
     serversocket.listen(5)
     print(f"Waiting for client to receive data ..")
@@ -109,11 +138,14 @@ def main():
         file_encrypted = clientsocket.recv(1).decode('utf-8')
         file_name = clientsocket.recv(100).decode('utf-8')
 
-        print("\nFile type: " + file_type)
-        print("Pickling: " + file_pickling)
-        print("File encrypted: " + file_encrypted)
-        print("Filename: " + file_name)
+        if not filetype:
+            print("[Server] \nFile type: " + file_type)
+            print("[Server] Pickling: " + file_pickling)
+            print("[Server] File encrypted: " + file_encrypted)
+            print("[Server] Filename: " + file_name)
         
+        if filetype:
+            file_name = os.path.join("server", file_name)
         #Delete previous file
         if os.path.exists(file_name):
             os.remove(file_name)
@@ -141,42 +173,59 @@ def main():
         elif output_type.lower() == 'c':
             # User choose to print on console
             if file_pickling == "1": # for json format pick class dictionary
-                data_json = json_dict(file_name)
+                data_json = json_dict(file_name)                
                 with open(file_name, 'r') as f:
                     c = f.read()
-                print(type(data_json))
-                print("Contents of data: \n{}".format(c))
+                if not filetype:
+                    print(type(data_json))
+                    print("Contents of data: \n{}".format(c))  
+                else:
+                    result_string["Unencrypted"] = format(c)
+
                 if os.path.exists(file_name):
                     os.remove(file_name)
 
             elif file_pickling == "2":  # for binary format pick class dictionary
                 data_binary = binary_dict(file_name)
-                print(type(data_binary)) # type class
-                print("Contents of data:  \n ",data_binary) # print data
+                if not filetype:
+                    print(type(data_binary)) # type class
+                    print("Contents of data:  \n ",data_binary) # print data                      
+                else:
+                    result_string["Unencrypted"] = data_binary
+                    #print("[Server] test result string 1: ", result_string["Unencrypted"] ) 
+
                 if os.path.exists(file_name):
                     os.remove(file_name)
-
             elif file_pickling == "3":  # for xml format pick class dictionary
                 xml_file = xml_format_dict(file_name)
-                print(type(xml_file)) # type class
-                print("Contents of data:  \n ",xml_file) # print data
+                if not filetype:
+                    print(type(xml_file)) # type class
+                    print("Contents of data:  \n ",xml_file) # print data
+                else:
+                    result_string["Unencrypted"] = xml_file
+                    #print("[Server] test result string 1: ", result_string["Unencrypted"] ) 
+
                 if os.path.exists(file_name):
                     os.remove(file_name)
-
             elif file_type == "t":
-                print("Contents of text file:")
-                output_console(file_name)
-                
+                if not filetype:
+                    print("Contents of text file:")
+                    output_console(file_name)
+                else:
+                    result_string["Unencrypted"] = file_name
+                    #print("[Server] test result string 1: ", result_string )                
 
         #Decrypt file if required
         if file_encrypted == "y":
             decrypt_file(file_name, output_type)
-        if file_type == "t" and output_type == "c":
+        if file_type == "t" and output_type == "c" and not filetype:
             os.remove(file_name)
 
         clientsocket.close()
         print("\nClosing server")
         break
+    #print("[Server] test result string 2: ", result_string )
+    return result_string
 
 if __name__ == "__main__":
-    main()
+    main("")
